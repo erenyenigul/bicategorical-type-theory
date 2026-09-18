6e92d4bc54428c49a1bb1c5696cf79e02405e947
Eren Yenigül <erenyenigul@gmail.com>
2026-09-18 14:51:34 +0200

    feat: compilable checkpoint. normalization for substitutions.

--- bin/normal.ml
+++ bin/normal.ml
@@ -1,6 +1,6 @@
 open Syntax
 
-type nf_context = (string * nf_ty) list
+type nf_context = nf_ty list
 
 and nf_substitution_gen = {
   name     : string;
@@ -44,6 +44,7 @@ and nf_term_gen = {
 }
 
 and nf_term = {
+  context: nf_context;
   domain: nf_ty;
   codomain: nf_ty;
   gens: nf_term_gen list
@@ -62,6 +63,7 @@ and nf_term_reduction_slice = {
 }
 
 and nf_term_reduction = {
+  context: nf_context;
   domain: nf_term;
   codomain: nf_term;
   slices: nf_term_reduction_slice list
@@ -75,22 +77,98 @@ and subst_nf_ty (s: nf_ty) (sub: nf_substitution) : nf_ty =
   match s with
   | NfBaseTy (name, l) -> NfBaseTy (name, List.map (fun t -> subst_nf_term t sub) l)
 
+and subst_nf_term_gen (gen: nf_term_gen) (sub: nf_substitution) : nf_term_gen = 
+  { 
+    name = gen.name; 
+    context = sub.codomain; 
+    domain = subst_nf_ty gen.domain sub; 
+    codomain = subst_nf_ty gen.codomain sub; 
+  }
+
 and subst_nf_term (t: nf_term) (sub: nf_substitution) : nf_term = 
-  { t with domain = subst_nf_ty t.domain sub; codomain = subst_nf_ty t.codomain sub }
+  { 
+    context = sub.domain; 
+    domain = subst_nf_ty t.domain sub; 
+    codomain = subst_nf_ty t.codomain sub; 
+    gens = List.map (fun g -> subst_nf_term_gen g sub) t.gens
+  }
+
+and normalize_term : term -> nf_term = function
+  | Id (ctx, ty) -> 
+    let nf_ctx = normalize_context ctx in
+    let nf_ty = normalize_ty ty in
+    { context = nf_ctx; domain = nf_ty; codomain = nf_ty; gens = [] }
+  | Var (s, ctx, ty1, ty2) ->
+    let nf_ctx = normalize_context ctx in
+    let nf_ty1 = normalize_ty ty1 in
+    let nf_ty2 = normalize_ty ty2 in
+    { 
+      context = nf_ctx; 
+      domain = nf_ty1; codomain = nf_ty2; 
+      gens = [{
+        name = s; 
+        context = nf_ctx; 
+        domain = nf_ty1; codomain = nf_ty2 
+      }] 
+    }
+  | Compose (t1, t2) ->
+    let nf_t1 = normalize_term t1 in
+    let nf_t2 = normalize_term t2 in
+    { 
+      context = nf_t1.context; 
+      domain = nf_t1.domain; 
+      codomain = nf_t2.codomain; 
+      gens = nf_t1.gens @ nf_t2.gens 
+    }
+  | SubTm (t, sub) ->
+    let nf_t = normalize_term t in
+    let nf_sub = normalize_substitution sub in
+    subst_nf_term nf_t nf_sub
 
 
-let rec normalize_context (ctx: context) : nf_context = match ctx with
+and normalize_context (ctx: context) : nf_context = match ctx with
   | Nil -> []
-  | Extend (ctx', s, t) -> (s, normalize_ty t) :: normalize_context ctx'
+  | Extend (t, ctx') -> (normalize_ty t) :: normalize_context ctx'
 
-let rec normalize_substitution (sub: substitution) : nf_substitution = match sub with
-  | Id ctx -> []
-  | Var (s, ctx1, ctx2) -> [(s, normalize_context ctx1, normalize_context ctx2)]
-  | Compose (sub1, sub2) -> normalize_substitution sub1 @ normalize_substitution sub2
+and normalize_substitution (sub: substitution) : nf_substitution = match sub with
+  | Id ctx -> 
+    let nf_ctx = normalize_context ctx in
+    { domain = nf_ctx; codomain = nf_ctx; gens = [] }
 
-let rec normalize_substitution_reduction (red: substitution_reduction) : nf_substitution_reduction = match red with
-  | Id sub -> []
-  | Var (s, sub1, sub2) -> [(normalize_substitution sub1, s, normalize_substitution sub2)]
-  | Compose (red1, red2) -> normalize_substitution_reduction red1 @ normalize_substitution_reduction red2
-  | LeftWhisker (sub, red) -> normalize_substitution_reduction red
-  | RightWhisker (red, sub) -> normalize_substitution_reduction red
+  | Var (s, ctx1, ctx2) ->
+    let nf_ctx1 = normalize_context ctx1 in
+    let nf_ctx2 = normalize_context ctx2 in
+    { 
+      domain = nf_ctx1; codomain = nf_ctx2; 
+      gens = [
+        { name = s; domain = nf_ctx1; codomain = nf_ctx2; }
+      ] 
+    }
+
+  | Compose (sub1, sub2) ->
+    let nf_sub1 = normalize_substitution sub1 in
+    let nf_sub2 = normalize_substitution sub2 in
+    { 
+      domain = nf_sub1.domain; 
+      codomain = nf_sub2.codomain;
+      gens = nf_sub1.gens @ nf_sub2.gens
+    }
+
+  | Empty ctx ->
+    let nf_ctx = normalize_context ctx in
+    { domain = nf_ctx; codomain = []; gens = [] }
+
+  | Weaken (ctx, ty) ->
+    let nf_ctx = normalize_context ctx in
+    let nf_ty = normalize_ty ty in
+    let nf_ctx' = nf_ty :: nf_ctx in
+
+    {
+      domain = nf_ctx; 
+      codomain = nf_ctx'; 
+      gens = [
+        { name = "PI" ; domain = nf_ctx; codomain = nf_ctx' }
+      ];
+    }
+
+  
--- bin/syntax.ml
+++ bin/syntax.ml
@@ -1,11 +1,13 @@
 type context = 
   | Nil 
-  | Extend of context * string * ty
+  | Extend of ty * context
   
 and substitution =
   | Id of context
   | Var of string * context * context
   | Compose of substitution * substitution
+  | Empty of context                     
+  | Weaken of context * ty 
 
 and substitution_reduction =
   | Id of substitution
@@ -31,148 +33,3 @@ and term_reduction =
   | LeftWhisker of term * term_reduction
   | RightWhisker of term_reduction * term
   | SubRed of term_reduction * substitution
-
-module Context = struct
-  type t = context
-  
-  let name_in_use (ctx: t) (s: string) : bool = 
-    let rec aux = function
-      | Nil -> false
-      | Extend (ctx', s', _) -> s = s' || aux ctx'
-    in aux ctx
-
-  let empty : t = Nil
-  let rec extend (ctx: t) (s: string) (t: ty) : t = 
-    Extend (ctx, s, t)
-
-end
-
-module Ty = struct
-  type t = ty
-  
-  let base (s: string) (l: term list) : t = BaseTy (s, l)
-  let sub (t: t) (sub: substitution) : t = SubTy (t, sub)
-end
-
-module Substitution = struct
-  type t = substitution
-  
-  let rec domain : t -> context = function
-  | Id a -> a
-  | Var (_, a, _) -> a
-  | Compose (f, _) -> domain f
-
-  let rec codomain : t -> context = function
-  | Id a -> a
-  | Var (_, _, b) -> b
-  | Compose (_, g) -> codomain g
-
-  let parallel (f: t) (g: t) : bool = domain f = domain g && codomain f = codomain g
-  let composable (f: t) (g: t) : bool = codomain f = domain g
-
-  let id (a: context) : t = Id a
-  let var (s: string) (a: context) (b: context) : t = 
-    Var (s, a, b)
-
-  let compose (f: t) (g: t) : t = 
-    if composable f g then Compose (f, g)
-    else failwith "compose: not composable"
-
-end
-
-module Term = struct
-  type t = term
-
-  let rec context : t -> context = function
-  | Id (ctx, _) -> ctx
-  | Var (_, ctx, _, _) -> ctx
-  | Compose (f, _) -> context f
-  | SubTm (_, s) -> Substitution.domain s
-
-  let rec domain : t -> ty = function
-  | Id (_, a) -> a
-  | Var (_, _, a, _) -> a
-  | Compose (f, _) -> domain f
-  | SubTm (t, s) -> SubTy (domain t, s)
-
-  let rec codomain : t -> ty = function
-  | Id (_, a) -> a
-  | Var (_, _, _, b) -> b
-  | Compose (_, g) -> codomain g
-  | SubTm (t, s) -> SubTy (codomain t, s)
-
-  let parallel (f: t) (g: t) : bool = context f = context g && domain f = domain g && codomain f = codomain g
-  let composable (f: t) (g: t) : bool = context f = context g && codomain f = domain g
-
-  let id (ctx: context) (a: ty) : t = Id (ctx, a)
-  let var (s: string) (ctx: context) (a: ty) (b: ty) : t = 
-    Var (s, ctx, a, b)
-
-  let compose (f: t) (g: t) : t = 
-    if composable f g then Compose (f, g)
-    else failwith "compose: not composable"
-end
-
-module SubstitutionReduction = struct
-  type t = substitution_reduction
-  let rec domain : t -> substitution = function
-  | Id a -> a
-  | Var (_, a, _) -> a
-  | Compose (f, _) -> domain f
-  | LeftWhisker (e, f) -> Substitution.compose e (domain f)
-  | RightWhisker (f, e) -> Substitution.compose (domain f) e
-
-  let rec codomain : t -> substitution = function
-  | Id a -> a
-  | Var (_, _, b) -> b
-  | Compose (_, g ) -> codomain g
-  | LeftWhisker (e, f) -> Substitution.compose e (codomain f)
-  | RightWhisker (f, e) -> Substitution.compose (codomain f) e
-
-  let composable (f: t) (g: t) : bool = codomain f = domain g
-
-  let id (a: substitution) : t = Id a
-  let var (s: string) (a: substitution) (b: substitution) : t = 
-    if Substitution.parallel a b then Var (s, a, b)
-    else failwith "var: not parallel"
-      
-  let compose (f: t) (g: t) : t = 
-    if composable f g then Compose (f, g)
-    else failwith "compose: not composable"
-
-  let left_whisker (e: substitution) (f: t) : t = LeftWhisker (e, f)
-  let right_whisker (f: t) (e: substitution) : t = RightWhisker (f, e)
-end
-
-module TermReduction = struct
-  type t = term_reduction
-  
-  let rec domain : t -> term = function
-  | Id a -> a
-  | Var (_, a, _) -> a
-  | Compose (f, _) -> domain f
-  | LeftWhisker (e, f) -> Term.compose e (domain f)
-  | RightWhisker (f, e) -> Term.compose (domain f) e
-
-  let rec codomain : t -> term = function
-  | Id a -> a
-  | Var (_, _, b) -> b
-  | Compose (_, g ) -> codomain g
-  | LeftWhisker (e, f) -> Term.compose e (codomain f)
-  | RightWhisker (f, e) -> Term.compose (codomain f) e
-
-  let composable (f: t) (g: t) : bool = codomain f = domain g
-
-  let id (a: term) : t = Id a
-  let var (s: string) (a: term) (b: term) : t = 
-    if Term.parallel a b then Var (s, a, b)
-    else failwith "var: not parallel"
-      
-  let compose (f: t) (g: t) : t = 
-    if composable f g then Compose (f, g)
-    else failwith "compose: not composable"
-
-  let left_whisker (e: term) (f: t) : t = LeftWhisker (e, f)
-  let right_whisker (f: t) (e: term) : t = RightWhisker (f, e)
-
-end
